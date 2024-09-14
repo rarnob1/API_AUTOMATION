2abe72db6feb75027d473f2c0a7b6b845073206d
rarnob1 <rarnob1@myseneca.ca>
2024-09-14 00:44:57 -0400

    get api and post api test case test

--- API DEMO/test_post_api.py
+++ API DEMO/test_post_api.py
@@ -9,10 +9,9 @@ payload={
     "name": "morpheus",
     "job": "leader"
 }
-
+##
 endpoint="https://reqres.in/api/users"
 def test_post_api():
     response =requests.post(endpoint,json=payload)
     assert response.status_code == 201,"response code is not 201 test failed"
     print(response.json())
-#test data
