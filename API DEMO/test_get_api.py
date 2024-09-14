1b304fa2aa96093ff709308fe4c437725e1334ed
rarnob1 <rarnob1@myseneca.ca>
2024-09-14 00:33:14 -0400

    get api and post api test case

---
+++ API DEMO/test_get_api.py
@@ -0,0 +1,19 @@
+import requests
+import pytest
+import json
+
+
+end_point="https://reqres.in/api/users/932"
+
+def test_getAPI():
+    response= requests.get(end_point)
+    assert response.status_code==200
+    #print(response.text)
+    print(response.json())
+    json_response=response.json()
+    # test email field value
+    assert 'email' in  json_response['data'],"email is missing"
+    assert json_response ['data']['email'] == 'janet.weaver@reqres.in' ,'value is missing'
+
+
+
---
+++ API DEMO/test_post_api.py
@@ -0,0 +1,17 @@
+from http.client import responses
+
+import requests
+import pytest
+import json
+
+
+payload={
+    "name": "morpheus",
+    "job": "leader"
+}
+
+endpoint="https://reqres.in/api/users"
+def test_post_api():
+    response =requests.post(endpoint,json=payload)
+    assert response.status_code == 201,"response code is not 201 test failed"
+    print(response.json())
